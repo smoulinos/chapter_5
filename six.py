f12b740c09eb38806e09616c11dddc49dca7e0a3
justishawkins <32522747+justishawkins@users.noreply.github.com>
2017-10-30 12:38:54 -0400

    1, 2, and 6 working now

--- six.py
+++ six.py
@@ -22,15 +22,17 @@ grade_list = [83, 75, 74.9, 70, 69.9, 65, 60, 59.9, 55, 50, 49.9, 45, 44.9, 40, 
 Test your function by printing the mark and the grade for all the elements in this list.
 """
 def mark_to_grade(num):
-    if num != 85:
-         "Great"
-    elif num >= 80:
-         "Good"
-    elif num >= 70:
-         "OK"
+    if num >= 90:
+         return "Great"
+    elif num >= 80 and num < 90:
+         return "Good"
+    elif num >= 70 and num < 80:
+         return"OK"
+    elif num >= 60 and num < 70:
+        return "Fair"
     elif num >= 50:
-        "Needs Help"
-    elif:
+        return "Needs Help"
+    else:
         return "Failing"
 grade_list = [83, 75, 74.9, 70, 69.9, 65, 60, 59.9, 55, 50, 49.9, 45, 44.9, 40, 39.9, 2, 0]
 
